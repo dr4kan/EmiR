aa6e8eee085b786c03ac384eebf2c8b07c57a91a
dr4kan <davide.pagano@cern.ch>
2024-06-28 15:01:28 +0200

    Update header

--- R/METHOD__Plot_history.R
+++ R/METHOD__Plot_history.R
@@ -1,6 +1,6 @@
 ###############################################################################
 # Emir: EmiR: Evolutionary minimization forR                                  #
-# Copyright (C) 2021 Davide Pagano & Lorenzo Sostero                          #
+# Copyright (C) 2021-2024 Davide Pagano & Lorenzo Sostero                     #
 #                                                                             #
 # This program is free software: you can redistribute it and/or modify        #
 # it under the terms of the GNU General Public License as published by        #
